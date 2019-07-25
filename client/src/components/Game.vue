3258eb476c84ca9aba6054e3f867b08b05ba797d
Filbert <filberttt@hotmail.com>
2019-07-25 17:35:27 +0700

    template

--- client/src/components/Game.vue
+++ client/src/components/Game.vue
@@ -6,7 +6,12 @@
                     <v-card style="height: 870px; width: 100%;" >
                         <v-layout align-end fill-height >
                             <v-flex>
-                                <h1 class="text-center">asd</h1>
+                                <v-layout>
+                                    
+                                </v-layout>
+
+
+
                                 <v-divider></v-divider>
                                 <v-card-actions>
                                     <v-btn style="margin-left: 44%; height: 80px; width: 80px;" fab color="green"><v-icon dark style="font-size: 40px;">done</v-icon></v-btn>
